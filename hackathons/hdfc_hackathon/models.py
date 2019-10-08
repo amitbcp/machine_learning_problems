b65874c8c60c8fe4874fae0c676399d9ff700e5b
Shivam Singla <shivam.singla@abzooba.com>
2019-10-08 13:47:51 +0530

    Added xgboost training method

--- hackathons/hdfc_hackathon/models.py
+++ hackathons/hdfc_hackathon/models.py
@@ -1,18 +1,17 @@
+"""
+Training models
+"""
 import os
-import pickle
 import time
 
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import tensorflow as tf
-from sklearn import metrics
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from tensorflow import keras
-from common.config_files.config import CGNConfigParser
+from sklearn import metrics
+from xgboost import XGBClassifier
 
+from common.config_files.config import CGNConfigParser
+from utils import model_save
 # from imblearn.over_sampling import SMOTE
 
 tf.keras.backend.clear_session()
@@ -123,3 +122,22 @@ def make_model2(features,
   model.compile(optimizer='adam', loss='binary_crossentropy', metrics=metrics)
 
   return model
+
+def xgboost_model(x_train, y_train, params=None):
+	"""
+	Trains a xgboost model and stores it in a file.
+
+	Args:
+		x_train: train dataset.
+		y_train: train labels.
+		params: input parameters for the model
+
+	Returns:
+		Filepath of the model file
+
+	Raises:
+		Exception: If dimensions of x_train and y_train donot match
+	"""
+	xgb_model = XGBClassifier(params)
+	xgb_model.fit(x_train, y_train)
+	return model_save(xgb_model)
